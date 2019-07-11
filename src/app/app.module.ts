6310dc6c9b99c3aa24c3c214facf9f9efb895557
NaughtyPhoton <nathanstrong.fx@gmail.com>
2019-07-10 18:31:44 -0700

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,12 @@ import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
-import { TrainingComponent } from './auth/training/training.component';
 import { CoachSessionComponent } from './coach/coach-session/coach-session.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, MaterialModule ],
-  declarations: [ AppComponent, LoginComponent, SignupComponent, TrainingComponent, CoachSessionComponent, WelcomeComponent ],
+  declarations: [ AppComponent, LoginComponent, SignupComponent, CoachSessionComponent, WelcomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
