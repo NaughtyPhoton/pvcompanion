a507798edb32ed687eaeceb0bcd0ac0c85bed7f4
NaughtyPhoton <nathanstrong.fx@gmail.com>
2019-07-10 18:02:39 -0700

    2 files modified, 9 added, 3 deleted

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'
+
+/*
+import { WelcomeComponent } from './welcome/welcome.component'
+import { MatchesComponent } from './matches/matches.component'
+import { LiveStreamComponent } from './live-stream/live-stream.component'
+
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'matches', component: MatchesComponent },
+  { path: 'livestream', component: LiveStreamComponent },
+]
+*/
+
+const routes: Routes = [
+  { path: '', component: WelcomeComponent }
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CameraComponent } from './camera/camera.component';
 import { MaterialModule } from './material/material.module';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { TrainingComponent } from './auth/training/training.component';
+import { CoachSessionComponent } from './coach/coach-session/coach-session.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, MaterialModule ],
-  declarations: [ AppComponent, CameraComponent ],
+  declarations: [ AppComponent, LoginComponent, SignupComponent, TrainingComponent, CoachSessionComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
